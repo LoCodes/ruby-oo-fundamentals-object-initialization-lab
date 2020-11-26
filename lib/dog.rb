cb39fff1a4e128aebe2192132f69610817c9e715
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-26 12:16:00 -0800

    commit tree 10798ab540904a285fe2796437b3df1e28e16c63

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+    def initialize(name, breed = "Mutt")
+        @name = name 
+        @breed = breed
+    end 
+
+
+end 
+
+# Dog
+#   #initialize
+#     sets the name of the dog in an instance variable @name
+#     sets the breed of the dog in an instance variable @breed
+#     defaults the breed argument to "Mutt" in an instance variable @breed
